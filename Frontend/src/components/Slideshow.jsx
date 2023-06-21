ee5c99bcab1de311935a92e3f64ff10551a4f5d5
dinesh07sengar <sengardante@gmail.com>
2023-06-21 12:02:56 +0530

    updated

--- Frontend/src/components/Slideshow.jsx
+++ Frontend/src/components/Slideshow.jsx
@@ -5,23 +5,28 @@ const Slideshow = () => {
     const slides = [
         {
             id: 1,
-            image: "https://img.cdn.zostel.com/cache/_59a0044cad4d16d4f4075657c2adb902/5_1_1400x600.jpg"
+            image: "https://img.cdn.zostel.com/cache/_59a0044cad4d16d4f4075657c2adb902/5_1_1400x600.jpg",
+            title:"Memory"
         },
         {
             id: 2,
-            image: "https://img.cdn.zostel.com/cache/_0c130a41e670628551785b66a116ca0f/048_Exterior_04_1400x600.jpg"
+            image: "https://img.cdn.zostel.com/cache/_0c130a41e670628551785b66a116ca0f/048_Exterior_04_1400x600.jpg",
+            title:"It's a Race"
         },
         {
             id: 3,
-            image: "https://img.cdn.zostel.com/cache/_606bf1e904105f3f61aa7abae817ba89/2_1400x600.jpg"
+            image: "https://img.cdn.zostel.com/cache/_606bf1e904105f3f61aa7abae817ba89/2_1400x600.jpg",
+            title:"If i write you"
         },
         {
             id: 4,
-            image: "https://img.cdn.zostel.com/cache/_bf6718c9924a17253c3e7a8bcc4640d8/Landing_Page_Photo_1400x600.jpg"
+            image: "https://img.cdn.zostel.com/cache/_bf6718c9924a17253c3e7a8bcc4640d8/Landing_Page_Photo_1400x600.jpg",
+            title:"What We Do When..."
         },
         {
             id: 5,
-            image: "https://img.cdn.zostel.com/cache/_1702bd3ab45e20acabe0c783694e8c73/4_1400x600.jpg"
+            image: "https://img.cdn.zostel.com/cache/_1702bd3ab45e20acabe0c783694e8c73/4_1400x600.jpg",
+            title:"Don't Be Stupid"
         }
     ];
 
@@ -49,7 +54,7 @@ const Slideshow = () => {
                     animate={{ x: `${-animation}%` }}
                     transition={{ duration: 0.5 }}
                 >
-                    <h2>heading</h2>
+                    <h2>{slide.title}</h2>
                     <img src={slide.image} alt={slide.title} />
                 </motion.div>
             ))}
